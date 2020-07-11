ce5f0cb7346d605afd417be373fdb08ff01b2e6e
deep_bohara <dpak23bohara@gmail.com>
2020-07-11 10:25:07 -0500

    Seller product list and details. Sale history on progress

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,4 +1,13 @@
 package domain;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public class Product {
+    private Long id;
+    private String productName;
+    private double price;
+    private String description;
+    private ProductCategory category;
+    private int quantity;
+    private MultipartFile productImage;
 }
